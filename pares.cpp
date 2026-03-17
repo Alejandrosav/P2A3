a5b32431ccea97165b3a1d85157ee349623169e2
Alejandrosav <sandovalveganenes@gmail.com>
2026-03-16 21:09:56 -0600

    New file

---
+++ pares.cpp
@@ -0,0 +1,16 @@
+// Alejandro Sandoval
+// 00625910
+// Ing. Tecnologias de la informacion
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int npar;
+
+    for(npar = 1; nropar <= 20; npar++){
+        cout << npar << endl;
+    }
+  
+}
